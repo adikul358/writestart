38cc11b974ea39e5ce4859b8f8ff1328f2959fe4
Pranav Gupta <88652820+pranavgupta2603@users.noreply.github.com>
2023-10-22 15:54:00 +0530

    Add files via upload

---
+++ chat.py
@@ -0,0 +1,13 @@
+import openai
+
+def chat(inp, message_history, role="user"):
+    message_history=[]
+    message_history.append({"role": role, "content": f"{inp}"})
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=message_history
+    )
+    reply_content = completion.choices[0].message.content
+    message_history.append({"role": "assistant", "content": f"{reply_content}"})
+    return reply_content
+
