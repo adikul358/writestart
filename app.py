1d306757ea1dc358f51ab41c7716088a8b7e7cfd
Pranav Gupta <pranavgupta2603@gmail.com>
2023-10-28 12:25:30 +0530

    working

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request, jsonify
 from dotenv import load_dotenv
+
+load_dotenv()
+
 from models.user_info import UserInfo
 
 from services.dynomodb import User
